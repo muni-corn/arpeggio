678b2aebef6ad4046e691da6716b9424be17d774
municorn <municorn@musicaloft.com>
2022-06-15 20:23:06 -0500

    DESTROY EVERYTHING AND START OVER
    lol :)

--- src/config.rs
+++
@@ -1,8 +0,0 @@
-use crate::palette::Palette;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-#[derive(Debug, Default, Deserialize, Serialize)]
-pub struct Config {}
-
-pub type Palettes = HashMap<String, Palette>;
