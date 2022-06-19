3d6455382ad66ceaf4c8964455b766408012871b
municorn <municorn@musicaloft.com>
2022-06-18 21:27:06 -0500

    Add basic logic
    with some todos :)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, io::Write};
 use clap::Parser;
 use image::DynamicImage;
 use log::{debug, info};
-use palette::{rgb::Rgb, Lab};
+use palette::rgb::Rgb;
 use serde::{Deserialize, Serialize};
 
 #[derive(Hash, PartialEq, Eq, Deserialize, Serialize)]
@@ -58,5 +58,28 @@ struct ArpeggioOpts {
 }
 
 fn main() {
-    println!("my little function")
+    use image::io::Reader as ImageReader;
+
+    simplelog::SimpleLogger::init(log::LevelFilter::Off, simplelog::Config::default()).unwrap();
+
+    debug!("parsing options");
+    let opts = ArpeggioOpts::parse();
+
+    // open output file early so we can fail early if we need to
+    let mut out_file =
+        std::fs::File::open(&opts.output).expect("couldn't open output file for writing");
+
+    info!("opening image '{}'", opts.input);
+    let img = ImageReader::open(&opts.input).unwrap().decode().unwrap();
+
+    info!("generating palette");
+    let palette = make_palette(img);
+
+    info!("writing palette to '{}'", opts.output);
+    let toml_str = toml::to_string_pretty(&palette).expect("couldn't format palette as toml");
+    write!(out_file, "{toml_str}").expect("couldn't write output to file");
+}
+
+fn make_palette(src_img: DynamicImage) -> Palette {
+    todo!()
 }
